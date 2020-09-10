be99c92486866f77f380d3f2d76711a794bdcd0a
foersterIDS <foerster@ids.uni-hannover.de>
2020-09-10 16:09:37 +0200

    fix error of incomplete jacobian in merge_residuals

--- src/merge_residuals.m
+++ src/merge_residuals.m
@@ -8,8 +8,18 @@ function [varargout] = merge_residuals(fun,res_arle,x,x_all,ds,Opt)
     if Opt.jacobian
         [R1,J1] = fun(x(1:end-1),x(end));
         [R2,J2] = res_arle(x,x_all,ds);
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        % There should be a better was to do this!
+        if length(J1(:,1))==length(J1(1,:))
+            delta = 10^-8;
+            R1p = fun(x(1:end-1),x(end)+delta);
+            J1l = (R1p-R1)*1/delta;
+        else
+            J1l = [];
+        end
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
         varargout{1} = [R1;R2];
-        varargout{2} = [J1;J2];
+        varargout{2} = [J1,J1l;J2];
     else
         R1 = fun(x(1:end-1),x(end));
         R2 = res_arle(x,x_all,ds);
