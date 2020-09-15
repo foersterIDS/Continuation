788a37dac6ed5b92b0ed8cc6c63a98326b2af9c9
foersterIDS <foerster@ids.uni-hannover.de>
2020-09-15 09:27:50 +0200

    problem in ellipsoid2

--- src/residual_arclength_ellipsoid2.m
+++ src/residual_arclength_ellipsoid2.m
@@ -10,9 +10,20 @@ function [residual,jacobian] = residual_arclength_ellipsoid2(x,xs,ds)
         f = ones(size(xs(:,1)));
     else
         v0 = 10^-15;
-        max_mag = max(abs(xs(:,end)-xs(:,end-1))+v0);
-        f = (max_mag./(abs(xs(:,end)-xs(:,end-1))+v0));
+        adxs = abs(xs(:,end)-xs(:,end-1));
+        max_mag = max(adxs+v0);
+        min_mag = min([adxs(adxs>0);v0]);
+        nzadxs = adxs;
+        nzadxs(nzadxs==0) = min_mag;
+        f = (max_mag./(nzadxs+v0));
     end
     residual = sum((f.*(x-xs(:,end))).^2./r.^2)-1;
     jacobian = (2*(f.*(x-xs(:,end)))./r.^2)';
+    
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    % So laeuft sich das ganze schnell fest. Gibt es eine bessere Loesung?
+    % Kann man das Residuum oder auch die Unbekannten eventuell noch anders
+    % skalieren?
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    
 end
