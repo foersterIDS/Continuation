ea48ae83984519a499eda368441c26ff2c0ff8d0
foersterIDS <foerster@ids.uni-hannover.de>
2020-09-22 17:21:24 +0200

    update residual_arclength_ellipsoid2

--- src/residual_arclength_ellipsoid2.m
+++ src/residual_arclength_ellipsoid2.m
@@ -6,16 +6,20 @@
 %
 function [residual,jacobian] = residual_arclength_ellipsoid2(x,xs,ds)
     r = ds;
+    fm = 10^6;
     if length(xs(1,:))==1
         f = ones(size(xs(:,1)));
     else
-        v0 = 10^-15;
+        v0 = 10^-15*ones(size(xs(:,end)));
+        axs = abs(xs(:,end));
         adxs = abs(xs(:,end)-xs(:,end-1));
+        v0(adxs==0) = axs(adxs==0)./10;
         max_mag = max(adxs+v0);
-        min_mag = min([adxs(adxs>0);v0]);
-        nzadxs = adxs;
-        nzadxs(nzadxs==0) = min_mag;
-        f = (max_mag./(nzadxs+v0));
+        nzadxs = adxs+v0;
+        f = (max_mag./nzadxs);
+        if max(f)~=1
+            f = (fm-1)/(max(f)-1)*(f-1)+1;
+        end
     end
     residual = sum((f.*(x-xs(:,end))).^2./r.^2)-1;
     jacobian = (2*(f.*(x-xs(:,end)))./r.^2)';
