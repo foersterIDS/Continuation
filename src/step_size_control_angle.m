e8fd6340deecf1f34a1bf3621e61405e225e0e92
tiddlesq <69509535+tiddlesq@users.noreply.github.com>
2020-10-08 15:27:21 +0200

    minor changes in step_size_control_angle and continuation_input

--- src/step_size_control_angle.m
+++ src/step_size_control_angle.m
@@ -8,40 +8,40 @@
 function [dsn] = step_size_control_angle(ds,ds0,error_counter,solver_output,do_deflate,vars,ls,Opt)
     %% calc angular change
     if length(ls) > 2
-%         vh = length(vars(:,1));
-%         theta = 0;
-%         
-%         % calculate greatest angle
-%         for k = 1:vh
-%             vr = vars(k,end-2:end);
-%             lr = ls(end-2:end);
-%             vec = [lr;vr];
-%             v1 = vec(:,end) - vec(:,end-1);
-%             v2 = vec(:,end-1) - vec(:,end-2);
-%             
-%             thetn = real(acos(max(min(dot(v1,v2)/(norm(v1)*norm(v2)),1),-1)));
-% 
-%             if thetn > theta
-%                 theta = thetn;
-%             end
-%         end
-        % Vorschlag für Winkelberechnung:
-        vec = [vars;ls];
-        v1 = vec(:,end) - vec(:,end-1);
-        v2 = vec(:,end-1) - vec(:,end-2);
-        theta = acos(dot(v1,v2)/(norm(v1)*norm(v2)));
+        vh = length(vars(:,1));
+        angle_max = 0;
+        
+        % calculate greatest angle
+        for k = 1:vh
+            vr = vars(k,end-2:end);
+            lr = ls(end-2:end);
+            vec = [lr;vr];
+            v1 = vec(:,end) - vec(:,end-1);
+            v2 = vec(:,end-1) - vec(:,end-2);
+            angle_n = vector_angle(v1,v2);
+
+            if angle_n > angle_max
+                angle_max = angle_n;
+            end
+        end
+        
+        angle = angle_max;
         
         % calculate ratio
-        ch = theta / Opt.step_size_angle;
+        ch = angle / Opt.step_size_angle;
         
         if ch < 1 % if ratio < 1, set ch to 1
             ch = 1;
-        elseif ch > 1.5 % high ratios are punished stronger (but ch_max = 5)
+        elseif ch > 1.5 && ch < 5 % high ratios are punished stronger (but ch_max = 5)
             ch = 5;
+        elseif ch > 5
+            warning('Point of high curvature!');
+            ch = 10;
         end
     else
         ch = 1;
     end
+    
     % calculate step size
     dsn = ds*Opt.n_iter_opt/(solver_output.iterations*ch);
     dsn = max(ds/2,dsn);
