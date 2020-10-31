ca9a7f08c2af74a4639058e964f69b30efc92ccc
tiddlesq <69509535+tiddlesq@users.noreply.github.com>
2020-10-31 15:47:20 +0100

    changes in interp_s

--- src/interp_s.m
+++ src/interp_s.m
@@ -4,37 +4,55 @@
 %   Leibniz University Hannover
 %   27.10.2020 - Tido Kubatschek
 %
-function [var_all_i, l_all_i, s_all_i] = interp_s(s_all, var_all, l_all, Opt)
-    if length(l_all) < 3
-        warning('for interpolation at least 3 data points are needed. Points are linearly interpolated now')
-    end
-    x_all = [var_all; l_all];
+function [s_all_i] = interp_s(s_all, var_all, l_all, Opt)
+%     var_all = vs;
+%     l_all = ls;
+%     s_all = 0;
+%     for k = 2:length(x_all)
+%         s_all = [s_all,s_all(end) + norm(x_all(:,k) - x_all(:,k-1))];
+%     end
+
+    n = length(l_all);
     
-    % calc new s with finer inc
-    s_all_i = linspace(s_all(1), s_all(end), Opt.interp_s_inc);
+    if n < 2
+        error('for interpolation at least 2 data points are needed.')
+    end
+    
+    x_all = [var_all; l_all];
     
     if Opt.interpolation_method.spline
         % use MATLAB intern function spline
-        pp = spline(s_all, x_all);
+        pp = @(s,x) spline(s, x);
 
     elseif Opt.interpolation_method.pchip
         % use MATLAB intern function pchip
-        pp = pchip(s_all, x_all);
+        pp = @(s,x) pchip(s, x);
         
     elseif Opt.interpolation_method.makima
         % use MATLAB intern function makima
-        pp = makima(s_all, x_all);
+        pp = @(s,x) makima(s, x);
         
     elseif Opt.interpolation_method.beziere
         warning('method has not been implemented yet! Using spline instead');
-        pp = spline(s_all, x_all);
+        pp = @(s,x) spline(s, x);
     else
         error('There is no such interpolation method.')
     end
     
-    x_all_i = ppval(pp, s_all_i);
+    % calc new s with finer inc
     
-    var_all_i = x_all_i(1:end-1,:);
-    l_all_i = x_all_i(end,:);
+    n_points = max([n, 4]);
+    
+    s_i= linspace(s_all(end-n_points), s_all(end), Opt.interp_s_inc);
+    x_all_i = ppval(pp(s_all, x_all), s_i);
+    
+    s_all_n = 0;
+    
+
+    for k = 2:length(x_all_i)
+        s_all_n = [s_all_n,s_all_n(end) + norm(x_all_i(:,k) - x_all_i(:,k-1))];
+    end
+    
+    
 end
 
