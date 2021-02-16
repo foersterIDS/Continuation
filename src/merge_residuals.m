b7413ae17d5a4a533451a3238dfb1e6b10e0f3d5
foersterIDS <foerster@ids.uni-hannover.de>
2021-02-16 17:16:06 +0100

    allow incomplete jacobian

--- src/merge_residuals.m
+++ src/merge_residuals.m
@@ -9,8 +9,9 @@ function [varargout] = merge_residuals(fun,res_arle,x,x_all,ds,Opt)
     if Opt.jacobian
         [R1,J1] = fun(x(1:end-1),x(end));
         [R2,J2] = res_arle(x,x_all,ds);
-        if ~diff(size(J1))
-            J1l = numeric_jacobian(@(x) fun(x(1:end-1),x(end)), x, 'derivative_dimensions', length(x), 'diffquot', Opt.diffquot);
+        [n11,n12] = size(J1);
+        if n12<=n11
+            J1l = numeric_jacobian(@(x) fun(x(1:end-1),x(end)), x, 'derivative_dimensions', (n12+1):numel(x), 'diffquot', Opt.diffquot);
         else
             J1l = [];
         end
