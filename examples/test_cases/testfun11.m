ffd50afefc98cbdc861941e3245fc8ab0f713d81
foersterIDS <foerster@ids.uni-hannover.de>
2021-02-26 14:20:52 +0100

    add testfun11

---
+++ examples/test_cases/testfun11.m
@@ -0,0 +1,9 @@
+addpath('test_cases\testfun11_aux');
+
+v0 = 10^-15;
+lams = 1;
+lame = 10;
+ds0 = 0.05;
+ds_max = 0.1;
+
+fun = @(v,l) residual_fun11(v,l);
---
+++ examples/test_cases/testfun11_aux/residual_fun11.m
@@ -0,0 +1,4 @@
+function [R,J] = residual_fun11(v,l)
+    R = v-10;
+    J = [1,0];
+end
