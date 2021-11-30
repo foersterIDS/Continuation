afd2a11936c817dd9882fb84fa8ed0e17b3ffe89
tiddlesq <69509535+tiddlesq@users.noreply.github.com>
2021-11-30 16:25:57 +0100

    Update continuation_input: check stepsize

--- src/continuation_input.m
+++ src/continuation_input.m
@@ -259,23 +259,29 @@ function [Opt,ds0] = continuation_input(varargin_cell,fun,var0,l_start,l_end,ds0
     %% check stepsize options
     %
     if Opt.check_step_size_options
+        %
         if ds0 < Opt.ds_min % ds0 must not be lower than ds_min
             error('ds0 cannot be smaller than ds_min. Consider adapting one of them.');
         end
-        if ds0 > Opt.ds_max
+        %
+        if ds0 > Opt.ds_max % ds0 must not be larger than ds_max
             error('ds0 cannot be larger than ds_max. Consider adapting one of them.');
         end
+        %
         if Opt.ds_min < 10*Opt.solver_tol % ds_min cannot be lower than tolerance of solver
-            warning('ds_min has to be at least 10*solver_tol = %.2e. ds_min is set to %.2e.',10*Opt.solver_tol,10*Opt.solver_tol);
             Opt.ds_min = 10*Opt.solver_tol;
+            warning('ds_min has to be at least 10*solver_tol = %.2e. ds_min has been adapted.',Opt.ds_min);
         end
+        %
         if ds0 > (sqrt(sum(var0.^2) + (l_end-l_start)^2)/10) % ds_max should not be larger than mag of points
             % get order of magnitude
             n_mag = floor(log10(sqrt(sum(var0.^2) + (l_end-l_start)^2)));
-            % order of magnitude must be at least 1 lower
-            warning('ds0 is too large! It must not be greater than 1e%i. ds0 is set to %.2e.',n_mag-1,10^(n_mag-1));
+            % adapt ds0
             ds0 = 10^(n_mag-1);
+            % order of magnitude must be at least 1 lower
+            warning('ds0 is too large! It must not be greater than 1.00e%i. ds0 has been adapted.',n_mag-1);
         end
+        %
     end
     %
 end
