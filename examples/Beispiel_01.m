fb0da1adad8b7b41e7188eb8055a5504b5c146c3
tiddlesq <69509535+tiddlesq@users.noreply.github.com>
2022-01-29 15:42:09 +0100

    Add ability to get a smoothened path

--- examples/Beispiel_01.m
+++ examples/Beispiel_01.m
@@ -24,3 +24,32 @@ testfun02; % Duffing: mu \ddot q + zeta \dot q + kappa q + \gamma q^3 = P cos( O
 %% Solve:
 [var_all,l_all,exitflag,bifs,s_all,last_jacobian,break_fun_out] = ...
     continuation(fun,v0,lams,lame,ds0,'bifurcation','mark','ds_max',ds_max,'plot','on','step_size_control','multiplicative');
+
+%% Smoothen path
+% [X_interp] = aux.smoothen_path([var_all; l_all], s_all);
+
+%% plot smooth curves into figure
+% plot_method = 'on'; % 'on' or 'three_dim'
+% 
+% l_interp = X_interp(end, :);
+% var_interp = X_interp(1:end-1,:);
+% 
+% if ~strcmp(plot_method, 'three_dim')
+%     num_pl = size(var_interp,1);
+% else
+%     num_pl = 1;
+% end
+% 
+% colors = cell(num_pl,1);        
+% for k = 1:num_pl
+%     colors(k) = {plot.get_RGB(k,num_pl+5,5)};
+% end
+% 
+% hold on;
+% if strcmp(plot_method, 'three_dim')
+%     pl = plot3(l_interp, var_interp(1,:), var_interp(2,:), 'LineStyle', ':', 'LineWidth', 2);
+%     set(pl, {'Color'}, colors);
+% else
+%     pl = plot(l_interp, var_interp, 'LineStyle', ':', 'LineWidth', 2);
+%     set(pl, {'Color'}, colors);
+% end
