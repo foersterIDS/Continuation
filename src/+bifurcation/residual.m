688706e1089b195cb09adf48f627c1dba4964241
foersterIDS <foerster@ids.uni-hannover.de>
2022-04-27 13:17:43 +0200

    update bifurcation.residual

--- src/+bifurcation/residual.m
+++ src/+bifurcation/residual.m
@@ -4,16 +4,16 @@
 %   Leibniz University Hannover
 %   27.10.2020 - Alwin Förster
 %
-function [R] = residual(fun,x,Opt,scale)
+function [R] = residual(fun,x,Opt,Info,scale)
     if Opt.jacobian
-        [R1,J1] = fun(x(1:end-1),x(end));
+        [R1,J1] = fun(x(1:Info.nv),x(Info.nv+1));
         if diff(size(J1))
-            J1 = J1(:,1:end-1);
+            J1 = J1(1:Info.nv,1:Info.nv);
         end
     else
         R1 = fun(x(1:end-1),x(end));
         fun_J = @(v) fun(v,x(end));
-        J1 = aux.numeric_jacobian(fun_J,x(1:end-1),R1,'diffquot',Opt.diffquot);
+        J1 = aux.numeric_jacobian(fun_J,x(1:Info.nv),R1,'diffquot',Opt.diffquot);
     end
     R2 = det(J1)*scale;
     R = [R1;R2];
