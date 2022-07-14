e824f67413b384d41ffe611f6decbe55784064e7
foersterIDS <foerster@ids.uni-hannover.de>
2022-07-14 11:19:31 +0200

    Update merge_residuals.m

--- src/+aux/merge_residuals.m
+++ src/+aux/merge_residuals.m
@@ -8,12 +8,7 @@
 function [varargout] = merge_residuals(func,res_corr,x,x_all,ds,last_jacobian,Opt)
     if Opt.jacobian
         [R1,J1] = func(x(1:end-1),x(end));
-        if Opt.corrector.orthogonal
-            [R2,J2] = res_corr(x,x_all,ds,last_jacobian);
-        else
-            [R2,J2] = res_corr(x,x_all,ds);
-        end
-        
+        [R2,J2] = res_corr(x,x_all,ds,last_jacobian);
         [n11,n12] = size(J1);
         if n12<=n11
             J1l = aux.numeric_jacobian(@(x) func(x(1:end-1),x(end)), x, 'derivative_dimensions', (n12+1):numel(x), 'diffquot', Opt.diffquot, 'central_value', R1);
@@ -24,11 +19,7 @@ function [varargout] = merge_residuals(func,res_corr,x,x_all,ds,last_jacobian,Op
         varargout{2} = [J1,J1l;J2];
     else
         R1 = func(x(1:end-1),x(end));
-        if Opt.corrector.orthogonal
-            R2 = res_corr(x,x_all,ds,last_jacobian);
-        else
-            R2 = res_corr(x,x_all,ds);
-        end
+        R2 = res_corr(x,x_all,ds,last_jacobian);
         varargout{1} = [R1;R2];
     end
 end
