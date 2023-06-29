e93b0d5a8cc5583f81b3b407db41dffaeecb1fb4
foersterIDS <foerster@ids.uni-hannover.de>
2023-06-29 16:59:25 +0200

    Add basic colors to plot.getRGB

--- src/+plot/getRGB.m
+++ src/+plot/getRGB.m
@@ -4,33 +4,54 @@
 %   Leibniz University Hannover
 %   25.11.2020 - Alwin Förster
 %
-function [ rgb ] = getRGB( value, maxvalue, minvalue, cmName )
+function [ rgb ] = getRGB( value, maxValue, minValue, cmName )
     value = value(:);
-    if nargin<4
-        cmName = 'plot.viridis';
-    elseif nargin<3
-        error('not enough input arguments');
-    elseif nargin>4
-        error('to many input arguments');
-    end
-    cm = colormap(cmName);
-    Pcm = linspace(0,1,numel(cm(:,1)))';
-    if maxvalue<minvalue
-        temp = minvalue;
-        minvalue = maxvalue;
-        maxvalue = temp;
-    end
-    if minvalue==maxvalue
-        if value>minvalue
-            P = 1;
-        elseif value<minvalue
-            P = 0;
-        else
-            P = 0.5;
+    if nargin==1 && isa(value,'char')
+        switch value
+            case 'r'
+                rgb = [183,39,58]/255;
+            case 'b'
+                rgb = [65,76,204]/255;
+            case 'g'
+                rgb = [65,163,52]/255;
+            case 'y'
+                rgb = [183,142,39]/255;
+            case 'e'
+                rgb = [181,0,24]/255;
+            case 'p'
+                rgb = [0,20,204]/255;
+            case 'k'
+                rgb = [0,0,0]/255;
+            otherwise
+                error('unknown color');
         end
     else
-        P = (value-minvalue)/(maxvalue-minvalue);
-        P = max([zeros(size(P)),min([ones(size(P)),P],[],2)],[],2);
+        if nargin<4
+            cmName = 'plot.viridis';
+        elseif nargin<3
+            error('not enough input arguments');
+        elseif nargin>4
+            error('to many input arguments');
+        end
+        cm = colormap(cmName);
+        Pcm = linspace(0,1,numel(cm(:,1)))';
+        if maxValue<minValue
+            temp = minValue;
+            minValue = maxValue;
+            maxValue = temp;
+        end
+        if minValue==maxValue
+            if value>minValue
+                P = 1;
+            elseif value<minValue
+                P = 0;
+            else
+                P = 0.5;
+            end
+        else
+            P = (value-minValue)/(maxValue-minValue);
+            P = max([zeros(size(P)),min([ones(size(P)),P],[],2)],[],2);
+        end
+        rgb = interp1(Pcm,cm,P);
     end
-    rgb = interp1(Pcm,cm,P);
 end
