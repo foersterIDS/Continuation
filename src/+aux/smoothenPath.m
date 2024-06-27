ef483f847f03787e8e927515c90fe6bae25ef61c
Tido Kubatschek <69509535+kubatschekIDS@users.noreply.github.com>
2024-06-27 12:58:58 +0200

    Update smoothenPath.m

--- src/+aux/smoothenPath.m
+++ src/+aux/smoothenPath.m
@@ -1,28 +1,5 @@
 %% path continuation - aux.smoothenPath
-%  Interpolates data to archieve a smoothened path. 
-%
-%
-%   Inputs:
-%       XData    -- contains a vector corresponding values or a 
-%                    matrix which rows are vectors of corresponding values 
-%       sData    -- contains sample points for interpolation 
-%
-%   Optional Inputs: (call by value pairs)
-%       'method'     -- interpolation method: e.g. linear, pchip, makima,
-%                       spline
-%       'number'     -- number of interpolation points. If passed,
-%                       increment cannot be also an input! sInterp is
-%                       calculated by linspace.
-%       'increment'  -- increment between interpolation points If passed,
-%                       number cannot be also an input! sInterp is
-%                       calculated by sStart:increment:sEnd.
-%
-%       --> By default 'method' is 'spline' and 'increment' is 1e-3.
-%
-%   Outputs:
-%       XInterp     -- interpolated XData
-%       sInterp     -- interpolated sData
-%
+%  Interpolates data to archieve a smoothened path.
 %
 %  See <a href="matlab:doc('interp1')">interp1</a> for detailed explanation of interpolation.
 %  See the <a href="matlab:open('..\doc\html\continuation.html')">documentation</a>.
@@ -31,94 +8,89 @@
 %   Leibniz University Hannover
 %   29.01.2022 - Tido Kubatschek
 % 
-function [XInterp,sInterp] = smoothenPath(XData, sData, varargin)
+function [varInterp,paraInterp,sInterp] = smoothenPath(varData, paraData, sData, NameValueArgs)
+    arguments
+        varData (:,:) double {mustBeReal}
+        paraData (:,:) double {mustBeReal}
+        sData (1,:) double {mustBeIncreasing(sData)} = []
+        NameValueArgs.number double {mustBeInteger,mustBeGreaterThan(NameValueArgs.number,0),mustBeScalarOrEmpty} = []
+        NameValueArgs.increment double {mustBeInteger,mustBeGreaterThan(NameValueArgs.increment,0),mustBeScalarOrEmpty} = []
+        NameValueArgs.interpolationMethod (1,:) char {mustBeMember(NameValueArgs.interpolationMethod, {'linear','pchip','makima','spline'})} = 'spline'
+        NameValueArgs.sTolerance (1,1) double {mustBeGreaterThan(NameValueArgs.sTolerance,0)} = 1e-2
+        NameValueArgs.maxIterations (1,1) double {mustBeInteger,mustBeGreaterThan(NameValueArgs.maxIterations,0)} = 3
+    end
+    %% get optional inputs
+    if ~isempty(NameValueArgs.number) && ~isempty(NameValueArgs.increment)
+        warning('You cannot specify both NameValueArgs.number and NameValueArgs.increment. Input for NameValueArgs.increment is used.')
+        numberOfInterpolationPoints = [];
+        interpolationIncrement = NameValueArgs.increment;
+    elseif ~isempty(NameValueArgs.number)
+        numberOfInterpolationPoints = NameValueArgs.number;
+        interpolationIncrement = [];
+    elseif isempty(NameValueArgs.number) && isempty(NameValueArgs.increment)
+        interpolationIncrement = 1e-3;
+        numberOfInterpolationPoints = [];
+    end
+    sTolerance = NameValueArgs.sTolerance;
+    maxIterations = NameValueArgs.maxIterations;
+    interpolationMethod = NameValueArgs.interpolationMethod;
     %
-    varLen = nargin - 2;
-    if numel(XData(1,:))>1
-        XLast = XData(:,end);
+    %% create expanded vector
+    %
+    nVar = size(varData,1);
+    xData = [varData; paraData];
+    %
+    %% get last solution point
+    if numel(xData(1,:))>1
+        XLast = xData(:,end);
     else
         XLast = [];
     end
+    %% initical calculation of stepsize
     %
-    if mod(varLen,2) ~= 0
-        error('There must be an even number of optional inputs!');
+    if isempty(sData)
+        sData = [0,cumsum(sqrt(sum((xData(:,2:end) - xData(:,1:end-1)).^2,1)))];
     end
     %
-    %% default options
+    %% iteration process of interpolation
     %
-    number = NaN;
-    increment = 1e-3;
-    method = 'spline';
-    %
-    %% check optional input
-    %
-    inputFlag = 0;
-    %
-    for k = 1:2:varLen
-        if strcmp(varargin{k}, 'number')
-            if ~(isnumeric(varargin{k+1}) && varargin{k+1} == round(varargin{k+1}) && varargin{k+1} > 0)
-                error('Wrong input for optional input number!');
-            elseif inputFlag ~= 0
-                error('You cannot specify number and increment!');
-            else
-                number = varargin{k+1};
-                increment = NaN;
-                inputFlag = 1;
-            end
-        elseif strcmp(varargin{k}, 'increment')
-            if ~(isnumeric(varargin{k+1}) && varargin{k+1} > 0)
-                error('Wrong input for optional input increment!');
-            elseif inputFlag ~= 0
-                error('You cannot specify number and increment!');
-            else
-                increment = varargin{k+1};
-                number = NaN;
-                inputFlag = 1;
-            end
-        elseif strcmp(varargin{k}, 'method')
-            if ~ischar(varargin{k+1})
-                error('Wrong input for optional input method!');
-            else
-                method = varargin{k+1};
-                if strcmp(method, 'cubic')
-                    warning('cubic doesnt work as the spacing is not equidistant, method is changed to spline');
-                    method = 'spline';
-                end
-            end
-        else
-            error('Wrong optional input!');
-        end
-    end
-    
-    %% Iteration der Interpolation
     diffS = inf;
-    tolS = 1e-6;
-    maxIt = 10;
     kIt = 0;
-
-    while diffS > tolS && kIt < maxIt
-        if ~isnan(number)
-            sInterp = linspace(sData(1), sData(end), number);
+    %
+    while diffS > sTolerance && kIt < maxIterations
+        if ~isempty(numberOfInterpolationPoints)
+            sInterp = linspace(sData(1), sData(end), numberOfInterpolationPoints);
         else
-            sInterp = sData(1):increment:sData(end);
+            sInterp = sData(1):interpolationIncrement:sData(end);
         end
-
+        %
         % Calculate interpolated points of curve
-        XInterp = (interp1(sData, XData.', sInterp, method)).';
-
+        xInterp = (interp1(sData, xData.', sInterp, interpolationMethod)).';
+        %
         % With those recalculate arclength
-        sInterp = [0,cumsum(sqrt(sum((XInterp(:,2:end) - XInterp(:,1:end-1)).^2,1)))];
-
+        sInterp = [0,cumsum(sqrt(sum((xInterp(:,2:end) - xInterp(:,1:end-1)).^2,1)))];
+        %
         % caculate difference in covered arclength
         diffS = abs(sData(end) - sInterp(end));
-
+        %
         % update
         sData = sInterp;
-        XData = XInterp;
-
+        xData = xInterp;
+        %
         kIt = kIt + 1;
     end
-    %% Last point
-    XInterp = [XInterp,XLast];
-    sInterp = [sInterp,norm(diff(XInterp(:,end+(-1:0)),1,2))];
+    % add Last point
+    xInterp = [xInterp,XLast];
+    sInterp = [sInterp,norm(diff(xInterp(:,end+(-1:0)),1,2))];
+    %% create output
+    varInterp = xInterp(1:nVar,:);
+    paraInterp = xInterp((nVar+1):end,:);
+end
+
+function mustBeIncreasing(array)
+    if any(diff(array) <= 0)
+        eidType = 'smoothenPath:notIncreasing';
+        msgType = 'Input must be an array of increasing values.';
+        error(eidType,msgType)
+    end
 end
